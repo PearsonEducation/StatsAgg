ca3ebef1a7bf9e445de905878e15b91d70bf8df9
prashant kumar <prashant.talkin@gmail.com>
2015-06-26 09:36:34 -0500

    statsAgg api layer: Adding junit test cases for the MetricGroups servlet.

---
+++ src/test/java/com/pearson/statsagg/webui/api/MetricGroupsTest.java
@@ -0,0 +1,79 @@
+/*
+ * Copyright 2015 UKUMAP5.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.pearson.statsagg.webui.api;
+
+import com.pearson.statsagg.database.metric_group.MetricGroupsDao;
+import javax.servlet.http.HttpServletRequest;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.junit.After;
+import static org.junit.Assert.assertEquals;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.mockito.Mockito;
+import static org.mockito.Mockito.atLeast;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ *
+ * @author prashant kumar (prashant4nov)
+ */
+public class MetricGroupsTest extends Mockito{
+    private static final JSONObject mockMetricGroupsJson = new JSONObject();
+    private static MetricGroupsDao metricGroupsDao;
+    private static final Logger logger = LoggerFactory.getLogger(MetricGroups.class.getName());
+
+    @BeforeClass
+    public static void setUp() {
+        JSONArray mockMetricGroupsList = new JSONArray();
+        JSONObject mockMetric = new JSONObject();
+        
+        mockMetric.put("name", "abcd");
+        mockMetric.put("id", "1");
+        mockMetricGroupsList.add(mockMetric);
+        
+        mockMetric.put("name", "xyz");
+        mockMetric.put("id", "2");
+        mockMetricGroupsList.add(mockMetric);
+             
+        mockMetricGroupsJson.put("metricgroups", mockMetricGroupsList);
+        mockMetricGroupsJson.put("count", 2);
+        metricGroupsDao = mock(MetricGroupsDao.class);
+        when(metricGroupsDao.getMetricGroups(10, 2)).thenReturn(mockMetricGroupsJson);
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    @Test
+    public void testgetMetricGroupsJson() throws Exception {
+        HttpServletRequest request = mock(HttpServletRequest.class);
+        when(request.getParameter("page_size")).thenReturn("2");
+        when(request.getParameter("page_number")).thenReturn("5");
+
+        MetricGroups metricGroups = new MetricGroups();
+        JSONObject result = metricGroups.getMetricGroupsJson(request, metricGroupsDao);
+
+        verify(request, atLeast(1)).getParameter("page_size");
+        verify(request, atLeast(1)).getParameter("page_number");
+        assertEquals(mockMetricGroupsJson, result);
+    }   
+}
