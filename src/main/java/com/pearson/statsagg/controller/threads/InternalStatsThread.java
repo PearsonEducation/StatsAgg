df8afd4223712d73e4ee3c3544364f10c5c44750
Jeffrey Schmidt <jeffrey.schmidt@pearson.com>
2016-08-09 21:19:58 -0500

    count number of metric datapoints in memory

--- src/main/java/com/pearson/statsagg/controller/threads/InternalStatsThread.java
+++ src/main/java/com/pearson/statsagg/controller/threads/InternalStatsThread.java
@@ -1,6 +1,10 @@
 package com.pearson.statsagg.controller.threads;
 
 import com.pearson.statsagg.globals.GlobalVariables;
+import com.pearson.statsagg.metric_aggregation.MetricTimestampAndValue;
+import com.pearson.statsagg.utilities.StackTrace;
+import java.util.List;
+import java.util.Map.Entry;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -13,10 +17,30 @@ public class InternalStatsThread implements Runnable {
     
     @Override
     public void run() {
+        calculateAverageNumberOfIncomingMetrics();
+        setNumberOfDatabaseInMemory();
+    }
+    
+    private void calculateAverageNumberOfIncomingMetrics() {
         long incomingMetricsRollingAverage = GlobalVariables.incomingMetricsCount.longValue() / 15;
         GlobalVariables.incomingMetricsRollingAverage.set(incomingMetricsRollingAverage);
-        
         GlobalVariables.incomingMetricsCount.set(0);
+    }
+    
+    private void setNumberOfDatabaseInMemory() {
+        long currentDatapointsInMemory_local = 0;
+        
+        for (Entry<String,List<MetricTimestampAndValue>> recentMetricTimestampsAndValuesByMetricKey_Entry : GlobalVariables.recentMetricTimestampsAndValuesByMetricKey.entrySet()) {
+            try {
+                List<MetricTimestampAndValue> metricTimestampAndValues = recentMetricTimestampsAndValuesByMetricKey_Entry.getValue();
+                currentDatapointsInMemory_local += metricTimestampAndValues.size();
+            }
+            catch (Exception e) {
+                logger.error(e.toString() + System.lineSeparator() + StackTrace.getStringFromStackTrace(e));
+            }
+        }
+        
+        GlobalVariables.currentDatapointsInMemory.set(currentDatapointsInMemory_local);
     }
     
 }
