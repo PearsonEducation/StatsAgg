04943b7fdcf4d49d98b6a4df4a5ba5c1349b6012
Jeffrey Schmidt <jeffrey.schmidt@pearson.com>
2016-08-09 21:26:49 -0500

    * json serialization annotations * new methods to generate json version of the object

--- src/main/java/com/pearson/statsagg/database_objects/metric_group/MetricGroup.java
+++ src/main/java/com/pearson/statsagg/database_objects/metric_group/MetricGroup.java
@@ -1,6 +1,17 @@
 package com.pearson.statsagg.database_objects.metric_group;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.annotations.SerializedName;
 import com.pearson.statsagg.database_engine.DatabaseObject;
+import com.pearson.statsagg.database_objects.metric_group_regex.MetricGroupRegex;
+import com.pearson.statsagg.database_objects.metric_group_tags.MetricGroupTag;
+import com.pearson.statsagg.utilities.StackTrace;
+import com.pearson.statsagg.webui.api.JsonOutputFieldNamingStrategy;
+import java.util.List;
 import org.apache.commons.lang3.builder.EqualsBuilder;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -12,10 +23,10 @@ public class MetricGroup extends DatabaseObject<MetricGroup> {
     
     private static final Logger logger = LoggerFactory.getLogger(MetricGroup.class.getName());
     
-    private Integer id_;
-    private String name_;
-    private String uppercaseName_ = null;
-    private String description_;
+    @SerializedName("id") private Integer id_;
+    @SerializedName("name") private String name_;
+    private transient String uppercaseName_ = null;
+    @SerializedName("description") private String description_;
     
     public MetricGroup() {
         this.id_ = -1;
@@ -30,7 +41,7 @@ public class MetricGroup extends DatabaseObject<MetricGroup> {
         this.name_ = name;
         this.uppercaseName_ = uppercaseName;
         this.description_ = description;
-    } 
+    }    
     
     @Override
     public boolean isEqual(MetricGroup metricGroup) {
@@ -61,6 +72,79 @@ public class MetricGroup extends DatabaseObject<MetricGroup> {
         metricGroupCopy.setDescription(metricGroup.getDescription());
         
         return metricGroupCopy;
+    }
+    
+    public static JsonObject getJsonObject_ApiFriendly(MetricGroup metricGroup) {
+        return getJsonObject_ApiFriendly(metricGroup, null, null);
+    }
+    
+    public static JsonObject getJsonObject_ApiFriendly(MetricGroup metricGroup, List<MetricGroupRegex> metricGroupRegexes, List<MetricGroupTag> metricGroupTags) {
+        
+        if (metricGroup == null) {
+            return null;
+        }
+        
+        try {
+            Gson metricGroup_Gson = new GsonBuilder().setFieldNamingStrategy(new JsonOutputFieldNamingStrategy()).setPrettyPrinting().create();   
+            JsonElement metricGroup_JsonElement = metricGroup_Gson.toJsonTree(metricGroup);
+            JsonObject jsonObject = new Gson().toJsonTree(metricGroup_JsonElement).getAsJsonObject();
+
+            JsonArray matchRegexes = new JsonArray();
+            JsonArray blacklistRegexes = new JsonArray();
+            JsonArray tags = new JsonArray();
+
+            if (metricGroupRegexes != null) {
+                for (MetricGroupRegex metricGroupRegex : metricGroupRegexes) {
+                    if ((metricGroupRegex.isBlacklistRegex() != null) && !metricGroupRegex.isBlacklistRegex() && (metricGroupRegex.getPattern() != null)) {
+                        matchRegexes.add(metricGroupRegex.getPattern());
+                    }
+                    else if ((metricGroupRegex.isBlacklistRegex() != null) && metricGroupRegex.isBlacklistRegex() && (metricGroupRegex.getPattern() != null)) {
+                        blacklistRegexes.add(metricGroupRegex.getPattern());
+                    }
+                }
+            }
+            
+            if (metricGroupTags != null) {
+                for (MetricGroupTag metricGroupTag : metricGroupTags) {
+                    if (metricGroupTag.getTag() != null) tags.add(metricGroupTag.getTag());
+                }
+            }
+            
+            jsonObject.add("match-regexes", matchRegexes);
+            jsonObject.add("blacklist-regexes", blacklistRegexes);
+            jsonObject.add("tags", tags);
+            
+            return jsonObject;
+        }
+        catch (Exception e) {
+            logger.error(e.toString() + System.lineSeparator() + StackTrace.getStringFromStackTrace(e));
+            return null;
+        }
+        
+    }
+    
+    public static String getJsonString_ApiFriendly(MetricGroup metricGroup) {
+        return getJsonString_ApiFriendly(metricGroup, null, null);
+    }
+    
+    public static String getJsonString_ApiFriendly(MetricGroup metricGroup, List<MetricGroupRegex> metricGroupRegexes, List<MetricGroupTag> metricGroupTags) {
+        
+        if (metricGroup == null) {
+            return null;
+        }
+        
+        try {
+            JsonObject jsonObject = getJsonObject_ApiFriendly(metricGroup, metricGroupRegexes, metricGroupTags);
+            if (jsonObject == null) return null;
+
+            Gson gson = new GsonBuilder().setPrettyPrinting().disableHtmlEscaping().create();   
+            return gson.toJson(jsonObject);
+        }
+        catch (Exception e) {
+            logger.error(e.toString() + System.lineSeparator() + StackTrace.getStringFromStackTrace(e));
+            return null;
+        }
+        
     }
     
     public Integer getId() {
