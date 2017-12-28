21287d4f2baab6aa8c9a1a913cfb558e2c29619a
Jeffrey Schmidt <jeffrey.schmidt@pearson.com>
2017-12-28 17:25:07 -0600

    force utf8

--- src/main/java/com/pearson/statsagg/webui/NotificationGroup_AlertAssociations.java
+++ src/main/java/com/pearson/statsagg/webui/NotificationGroup_AlertAssociations.java
@@ -70,7 +70,15 @@ public class NotificationGroup_AlertAssociations extends HttpServlet {
             return;
         }
         
-        response.setContentType("text/html");
+        try {  
+            request.setCharacterEncoding("UTF-8");
+            response.setCharacterEncoding("UTF-8");
+            response.setContentType("text/html");
+        }
+        catch (Exception e) {
+            logger.error(e.toString() + System.lineSeparator() + StackTrace.getStringFromStackTrace(e));
+        } 
+        
         PrintWriter out = null;
     
         String name = request.getParameter("Name");
