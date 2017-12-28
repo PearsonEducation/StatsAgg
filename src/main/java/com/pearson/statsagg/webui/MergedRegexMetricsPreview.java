40a665bdf5664fe445e27761f69f59e4c02e88e0
Jeffrey Schmidt <jeffrey.schmidt@pearson.com>
2017-12-28 17:28:03 -0600

    force utf8.

--- src/main/java/com/pearson/statsagg/webui/MergedRegexMetricsPreview.java
+++ src/main/java/com/pearson/statsagg/webui/MergedRegexMetricsPreview.java
@@ -67,7 +67,15 @@ public class MergedRegexMetricsPreview extends HttpServlet {
             return;
         }
         
-        response.setContentType("text/html");
+        try {  
+            request.setCharacterEncoding("UTF-8");
+            response.setCharacterEncoding("UTF-8");
+            response.setContentType("text/html");
+        }
+        catch (Exception e) {
+            logger.error(e.toString() + System.lineSeparator() + StackTrace.getStringFromStackTrace(e));
+        } 
+        
         PrintWriter out = null;
         
         TreeSet<String> matchRegexes = getMetricGroupNewlineDelimitedParameterValues(request, "MatchRegexes");
