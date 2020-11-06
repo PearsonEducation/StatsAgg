ea14873a628b486130bd5fb23dcc405650beff3f
Jeffrey Schmidt <jeffrey.schmidt@pearson.com>
2020-11-06 17:09:32 -0600

    rename package for consistency

---
+++ src/main/java/com/pearson/statsagg/database_objects/notification_groups/NotificationGroup.java
@@ -0,0 +1,204 @@
+package com.pearson.statsagg.database_objects.notification_groups;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.annotations.SerializedName;
+import com.pearson.statsagg.utilities.core_utils.StackTrace;
+import com.pearson.statsagg.database_objects.JsonOutputFieldNamingStrategy;
+import com.pearson.statsagg.utilities.db_utils.DatabaseObject;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import org.apache.commons.lang.StringUtils;
+import org.apache.commons.lang3.builder.EqualsBuilder;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * @author Jeffrey Schmidt
+ */
+public class NotificationGroup implements DatabaseObject<NotificationGroup>  {
+    
+    private static final Logger logger = LoggerFactory.getLogger(NotificationGroup.class.getName());
+
+    @SerializedName("id") private Integer id_;
+    @SerializedName("name") private String name_ = null;
+    private transient String uppercaseName_ = null;
+    @SerializedName("email_addresses") private String emailAddresses_ = null;
+    
+    public NotificationGroup() {
+        this.id_ = -1;
+    }
+    
+    public NotificationGroup(Integer id, String name, String emailAddresses) {
+        this(id, name, ((name == null) ? null : name.toUpperCase()), emailAddresses);
+    } 
+    
+    public NotificationGroup(Integer id, String name, String uppercaseName, String emailAddresses) {
+        this.id_ = id;
+        this.name_ = name;
+        this.uppercaseName_ = uppercaseName;
+        this.emailAddresses_ = emailAddresses;
+    }
+
+    @Override
+    public boolean isEqual(NotificationGroup notificationGroup) {
+        
+        if (notificationGroup == null) return false;
+        if (notificationGroup == this) return true;
+        if (notificationGroup.getClass() != getClass()) return false;
+        
+        return new EqualsBuilder()
+                .append(id_, notificationGroup.getId())
+                .append(name_, notificationGroup.getName())
+                .append(uppercaseName_, notificationGroup.getUppercaseName())
+                .append(emailAddresses_, notificationGroup.getEmailAddresses())
+                .isEquals();
+
+    }
+
+    public String getEmailAddressesCsv() {
+        
+        if (emailAddresses_ == null) {
+            return null;
+        }
+        
+        StringBuilder emailAddressesOutput = new StringBuilder();
+        
+        String[] emailAddresses = StringUtils.split(emailAddresses_, ",");
+        
+        if ((emailAddresses != null) && (emailAddresses.length != 0)) {
+            for (int i = 0; i < emailAddresses.length; i++) {
+                String trimmedEmailAddress = emailAddresses[i].trim();
+                emailAddressesOutput.append(trimmedEmailAddress);
+                if ((i + 1) != emailAddresses.length) emailAddressesOutput.append(", ");
+            }
+        }
+        
+        return emailAddressesOutput.toString();
+    }
+    
+    public List<String> getEmailAddressesList() {
+
+        if (emailAddresses_ == null) {
+            return new ArrayList<>();
+        }
+        
+        List<String> emailAddresses = new ArrayList<>();
+        
+        try {
+            String[] emailAddresses_Array = StringUtils.split(emailAddresses_, ",");
+            emailAddresses.addAll(Arrays.asList(emailAddresses_Array));
+        }
+        catch(Exception e) {
+            logger.error(e.toString() + System.lineSeparator() + StackTrace.getStringFromStackTrace(e));
+        }
+        
+        return emailAddresses;
+    }
+    
+    public static NotificationGroup copy(NotificationGroup notificationGroup) {
+        
+        if (notificationGroup == null) {
+            return null;
+        }
+        
+        NotificationGroup notificationGroupCopy = new NotificationGroup();
+        
+        notificationGroupCopy.setId(notificationGroup.getId());
+        notificationGroupCopy.setName(notificationGroup.getName());
+        notificationGroupCopy.setUppercaseName(notificationGroup.getUppercaseName());
+        notificationGroupCopy.setEmailAddresses(notificationGroup.getEmailAddresses());
+        
+        return notificationGroupCopy;
+    }
+    
+    public static JsonObject getJsonObject_ApiFriendly(NotificationGroup notificationGroup) {
+        
+        if (notificationGroup == null) {
+            return null;
+        }
+        
+        try {
+            Gson notificationGroup_Gson = new GsonBuilder().setFieldNamingStrategy(new JsonOutputFieldNamingStrategy()).setPrettyPrinting().create();   
+            JsonElement notificationGroup_JsonElement = notificationGroup_Gson.toJsonTree(notificationGroup);
+            JsonObject jsonObject = new Gson().toJsonTree(notificationGroup_JsonElement).getAsJsonObject();
+            
+            JsonArray emailAddresses_JsonArray = new JsonArray();
+            List<String> emailAddresses_List = notificationGroup.getEmailAddressesList();
+            
+            if (emailAddresses_List != null) {
+                for (String emailAddress : emailAddresses_List) {
+                    if (emailAddress == null) continue;
+                    emailAddresses_JsonArray.add(emailAddress.trim());
+                }
+            }
+                    
+            jsonObject.remove("email_addresses");
+            jsonObject.add("email_addresses", emailAddresses_JsonArray);
+            
+            return jsonObject;
+        }
+        catch (Exception e) {
+            logger.error(e.toString() + System.lineSeparator() + StackTrace.getStringFromStackTrace(e));
+            return null;
+        }
+        
+    }
+    
+    public static String getJsonString_ApiFriendly(NotificationGroup notificationGroup){
+        
+        if (notificationGroup == null) {
+            return null;
+        }
+        
+        try {
+            JsonObject jsonObject = getJsonObject_ApiFriendly(notificationGroup);
+            if (jsonObject == null) return null;
+
+            Gson gson = new GsonBuilder().setPrettyPrinting().disableHtmlEscaping().create();   
+            return gson.toJson(jsonObject);
+        }
+        catch (Exception e) {
+            logger.error(e.toString() + System.lineSeparator() + StackTrace.getStringFromStackTrace(e));
+            return null;
+        }
+        
+    }
+    
+    public Integer getId() {
+        return id_;
+    }
+    
+    public void setId(Integer id) {
+        this.id_ = id;
+    }
+
+    public String getName() {
+        return name_;
+    }
+
+    public void setName(String name) {
+        this.name_ = name;
+    }
+    
+    public String getUppercaseName() {
+        return uppercaseName_;
+    }
+
+    public void setUppercaseName(String uppercaseName) {
+        this.uppercaseName_ = uppercaseName;
+    }
+    
+    public String getEmailAddresses() {
+        return emailAddresses_;
+    }
+
+    public void setEmailAddresses(String emailAddresses) {
+        this.emailAddresses_ = emailAddresses;
+    }
+    
+}
---
+++ src/main/java/com/pearson/statsagg/database_objects/notification_groups/NotificationGroupsSql.java
@@ -0,0 +1,49 @@
+package com.pearson.statsagg.database_objects.notification_groups;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * @author Jeffrey Schmidt
+ */
+public class NotificationGroupsSql {
+
+    private static final Logger logger = LoggerFactory.getLogger(NotificationGroupsSql.class.getName());
+    
+    protected final static String Select_NotificationGroup_ByPrimaryKey = 
+                    "SELECT * FROM NOTIFICATION_GROUPS " +
+                    "WHERE ID = ?";
+    
+    protected final static String Select_NotificationGroup_ByName = 
+                    "SELECT * FROM NOTIFICATION_GROUPS " +
+                    "WHERE NAME = ?";
+    
+    protected final static String Select_NotificationGroup_Names_OrderByName = 
+                    "SELECT NAME FROM NOTIFICATION_GROUPS " +
+                    "WHERE NAME LIKE ? " +
+                    "ORDER BY NAME";
+    
+    protected final static String Select_AllNotificationGroups = 
+                    "SELECT * FROM NOTIFICATION_GROUPS";
+    
+    protected final static String Select_AllNotificationGroups_IdsAndNames = 
+                    "SELECT ID, NAME FROM NOTIFICATION_GROUPS";
+    
+    protected final static String Select_DistinctNotificationGroupIds = 
+                    "SELECT DISTINCT ID FROM NOTIFICATION_GROUPS";
+    
+    protected final static String Insert_NotificationGroup =
+                    "INSERT INTO NOTIFICATION_GROUPS " +
+                    "(NAME, UPPERCASE_NAME, EMAIL_ADDRESSES) " +
+                    "VALUES(?,?,?)";
+    
+    protected final static String Update_NotificationGroup_ByPrimaryKey =
+                    "UPDATE NOTIFICATION_GROUPS " +
+                    "SET NAME = ?, UPPERCASE_NAME = ?, EMAIL_ADDRESSES = ? " +
+                    "WHERE ID = ?";
+    
+    protected final static String Delete_NotificationGroup_ByPrimaryKey = 
+                    "DELETE FROM NOTIFICATION_GROUPS " +
+                    "WHERE ID = ?";
+    
+}
