ef86fc69dc168b1878fbe8b96535a36574a136f1
Jeffrey Schmidt <jeffrey.schmidt@pearson.com>
2021-02-25 09:29:21 -0600

    html field/label formatting tweaks

--- src/main/java/com/pearson/statsagg/web_ui/SuspensionDetails.java
+++ src/main/java/com/pearson/statsagg/web_ui/SuspensionDetails.java
@@ -138,27 +138,28 @@ public class SuspensionDetails extends HttpServlet {
             outputString.append("<div class=\"col-md-4 statsagg_three_panel_first_panel\">\n");
             outputString.append("<div class=\"panel panel-info\"> <div class=\"panel-heading\"><b>Core Details</b></div> <div class=\"panel-body statsagg_force_word_wrap\">");
             
-            outputString.append("<b>Name</b> = ").append(StatsAggHtmlFramework.htmlEncode(suspension.getName())).append("<br>");
+            outputString.append("<b>Name:</b> ").append(StatsAggHtmlFramework.htmlEncode(suspension.getName())).append("<br>");
             
-            outputString.append("<b>ID</b> = ").append(suspension.getId()).append("<br>");
+            outputString.append("<b>ID:</b> ").append(suspension.getId()).append("<br>");
             
             outputString.append("<br>");
             
-            outputString.append("<b>Description</b> = ");
+            outputString.append("<b>Description:</b> ");
             if (suspension.getDescription() != null) {
+                outputString.append("<br>");
                 String encodedAlertDescription = StatsAggHtmlFramework.htmlEncode(suspension.getDescription());
                 outputString.append(encodedAlertDescription.replaceAll("\n", "<br>")).append("<br><br>");
             }
             else outputString.append("<br><br>");
             
-            outputString.append("<b>Is Enabled?</b> = ");
+            outputString.append("<b>Is Enabled? :</b> ");
             String isEnabled = "No";
             if ((suspension.isEnabled() != null) && suspension.isEnabled()) isEnabled = "Yes";
             if (suspension.isEnabled() != null) outputString.append(isEnabled).append("<br>");
             else outputString.append("N/A <br>");
                 
             if ((suspension.getSuspendBy() != null) && (suspension.getSuspendBy() != Suspension.SUSPEND_BY_METRICS)) {
-                outputString.append("<b>Suspend notification only?</b> = ");
+                outputString.append("<b>Suspend Notification Only? :</b> ");
                 String isSuspendNotificationOnly = "No";
                 if ((suspension.isSuspendNotificationOnly() != null) && suspension.isSuspendNotificationOnly()) isSuspendNotificationOnly = "Yes";
                 if (suspension.isSuspendNotificationOnly() != null) outputString.append(isSuspendNotificationOnly).append("<br>");
@@ -169,26 +170,26 @@ public class SuspensionDetails extends HttpServlet {
             
             String isValid = "No";
             if (Suspension.isValid(suspension).isValid()) isValid = "Yes";
-            outputString.append("<b>Is suspension configuration valid?</b> = ").append(isValid).append("<br>");
+            outputString.append("<b>Is Suspension Configuration Valid? :</b> ").append(isValid).append("<br>");
             
             String isSuspensionInSuspensionTimeWindow = "No";
             if (Suspension.isSuspensionInSuspensionTimeWindow(suspension)) isSuspensionInSuspensionTimeWindow = "Yes";
-            outputString.append("<b>Is currently in the suspension window?</b> = ").append(isSuspensionInSuspensionTimeWindow).append("<br>");
+            outputString.append("<b>Is Currently In The Suspension Window? :</b> ").append(isSuspensionInSuspensionTimeWindow).append("<br>");
             
             String isSuspensionActive = "No";
             if (Suspension.isSuspensionActive(suspension)) isSuspensionActive = "Yes";
-            outputString.append("<b>Is suspension active?</b> = ").append(isSuspensionActive).append("<br>");
+            outputString.append("<b>Is Suspension Active? :</b> ").append(isSuspensionActive).append("<br>");
             
             if ((suspension.getSuspendBy() != null) && (suspension.getSuspendBy() != Suspension.SUSPEND_BY_METRICS)) {
                 outputString.append("<br>");
-                outputString.append("<b>Alert Associations</b> = ");            
+                outputString.append("<b>Alert Associations:</b> ");            
                 String alertsAssociationsPopup = "<a class=\"iframe cboxElement\" href=\"Suspension-AlertAssociations?ExcludeNavbar=true&amp;Name=" + StatsAggHtmlFramework.urlEncode(suspension.getName()) + "\">" + StatsAggHtmlFramework.htmlEncode(suspension.getName()) + "</a>";
                 outputString.append(alertsAssociationsPopup);  
             }
             
             if ((suspension.getSuspendBy() != null) && (suspension.getSuspendBy() == Suspension.SUSPEND_BY_METRICS)) {
                 outputString.append("<br>");
-                outputString.append("<b>Metric Associations</b> = ");            
+                outputString.append("<b>Metric Associations:</b> ");            
                 String metricKeyAssociationsPopup = "<a class=\"iframe cboxElement\" href=\"Suspension-MetricKeyAssociations?ExcludeNavbar=true&amp;Name=" + StatsAggHtmlFramework.urlEncode(suspension.getName()) + "\">" + StatsAggHtmlFramework.htmlEncode(suspension.getName()) + "</a>";
                 outputString.append(metricKeyAssociationsPopup);              
             }
@@ -200,9 +201,9 @@ public class SuspensionDetails extends HttpServlet {
             outputString.append("<div class=\"col-md-4 statsagg_three_panel_second_panel\">\n");
             outputString.append("<div class=\"panel panel-info\"> <div class=\"panel-heading\"><b>Suspension Type</b></div> <div class=\"panel-body statsagg_force_word_wrap\">");
             
-            outputString.append("<b>Suspend by...</b> = ");
+            outputString.append("<b>Suspend By... :</b> ");
             if ((suspension.getSuspendBy() != null) && (suspension.getSuspendBy() == Suspension.SUSPEND_BY_ALERT_ID)) outputString.append("Alert name").append("<br>");
-            else if ((suspension.getSuspendBy() != null) && (suspension.getSuspendBy() == Suspension.SUSPEND_BY_METRIC_GROUP_TAGS)) outputString.append("Metric group tags").append("<br>");
+            else if ((suspension.getSuspendBy() != null) && (suspension.getSuspendBy() == Suspension.SUSPEND_BY_METRIC_GROUP_TAGS)) outputString.append("Metric Group Tags").append("<br>");
             else if ((suspension.getSuspendBy() != null) && (suspension.getSuspendBy() == Suspension.SUSPEND_BY_EVERYTHING)) outputString.append("Everything").append("<br>");
             else if ((suspension.getSuspendBy() != null) && (suspension.getSuspendBy() == Suspension.SUSPEND_BY_METRICS)) outputString.append("Metrics").append("<br>");
             else outputString.append("N/A <br>");
@@ -210,7 +211,7 @@ public class SuspensionDetails extends HttpServlet {
             outputString.append("<br>");
 
             if ((suspension.getSuspendBy() != null) && (suspension.getSuspendBy() == Suspension.SUSPEND_BY_ALERT_ID)) {
-                outputString.append("<b>Alert name</b> = ");
+                outputString.append("<b>Alert Name:</b> ");
                 if (suspension.getAlertId() != null) {
                     Alert alert = AlertsDao.getAlert(DatabaseConnections.getConnection(), true, suspension.getAlertId());
 
@@ -224,7 +225,7 @@ public class SuspensionDetails extends HttpServlet {
             }
 
             if ((suspension.getSuspendBy() != null) && (suspension.getSuspendBy() == Suspension.SUSPEND_BY_METRIC_GROUP_TAGS)) {
-                outputString.append("<b>Metric group tags to include</b> = ");
+                outputString.append("<b>Metric Group Tags To Include:</b> ");
                 if ((suspension.getMetricGroupTagsInclusive() != null) && !suspension.getMetricGroupTagsInclusive().isEmpty()) {
                     String metricGroupTagsExclusiveFormatted = StringUtils.replace(StatsAggHtmlFramework.htmlEncode(suspension.getMetricGroupTagsInclusive()), "\n", "<br>&nbsp;&nbsp;&nbsp;");
                     outputString.append("<br>&nbsp;&nbsp;&nbsp;").append(metricGroupTagsExclusiveFormatted).append("<br>");
@@ -233,7 +234,7 @@ public class SuspensionDetails extends HttpServlet {
             }
             
             if ((suspension.getSuspendBy() != null) && (suspension.getSuspendBy() == Suspension.SUSPEND_BY_EVERYTHING)) {
-                outputString.append("<b>Metric group tags to exclude</b> = ");
+                outputString.append("<b>Metric Group Tags To Exclude:</b> ");
                 if ((suspension.getMetricGroupTagsExclusive() != null) && !suspension.getMetricGroupTagsExclusive().isEmpty()) {
                     String metricGroupTagsInclusiveFormatted = StringUtils.replace(StatsAggHtmlFramework.htmlEncode(suspension.getMetricGroupTagsExclusive()), "\n", "<br>&nbsp;&nbsp;&nbsp;");
                     outputString.append("<br>&nbsp;&nbsp;&nbsp;").append(metricGroupTagsInclusiveFormatted).append("<br>");
@@ -242,7 +243,7 @@ public class SuspensionDetails extends HttpServlet {
             }
             
             if ((suspension.getSuspendBy() != null) && (suspension.getSuspendBy() == Suspension.SUSPEND_BY_METRICS)) {
-                outputString.append("<b>Metric suspension regexes</b> = ");
+                outputString.append("<b>Metric Suspension Regexes:</b> ");
                 if ((suspension.getMetricSuspensionRegexes() != null) && !suspension.getMetricSuspensionRegexes().isEmpty()) {
                     String metricSuspensionRegexesFormatted = StringUtils.replace(StatsAggHtmlFramework.htmlEncode(suspension.getMetricSuspensionRegexes()), "\n", "<br>&nbsp;&nbsp;&nbsp;");
                     outputString.append("<br>&nbsp;&nbsp;&nbsp;").append(metricSuspensionRegexesFormatted).append("<br>");
@@ -257,28 +258,28 @@ public class SuspensionDetails extends HttpServlet {
             outputString.append("<div class=\"col-md-4 statsagg_three_panel_third_panel\">\n");
             outputString.append("<div class=\"panel panel-info\"> <div class=\"panel-heading\"><b>Suspension Schedule</b></div> <div class=\"panel-body statsagg_force_word_wrap\">");
             
-            outputString.append("<b>Suspension Type</b> = ");
+            outputString.append("<b>Suspension Type:</b> ");
             if ((suspension.isOneTime() != null) && suspension.isOneTime()) outputString.append("One time").append("<br>");
             else if ((suspension.isOneTime() != null) && !suspension.isOneTime()) outputString.append("Recurring").append("<br>");
             else outputString.append("N/A <br>");
             
             outputString.append("<br>");
 
-            outputString.append("<b>Start date</b> = ");
+            outputString.append("<b>Start Date:</b> ");
             if (suspension.getStartDate() != null) {
                 String startDateString = DateAndTime.getFormattedDateAndTime(suspension.getStartDate(), "yyyy-MM-dd");
                 outputString.append(startDateString).append("<br>");
             }
             else outputString.append("N/A <br>");
             
-            outputString.append("<b>Start time</b> = ");
+            outputString.append("<b>Start Time:</b> ");
             if (suspension.getStartTime() != null) {
                 String startTimeString = DateAndTime.getFormattedDateAndTime(suspension.getStartTime(), "h:mm a");
                 outputString.append(startTimeString).append("<br>");
             }
             else outputString.append("N/A <br>");
             
-            outputString.append("<b>Duration</b> = ");
+            outputString.append("<b>Duration:</b> ");
             if (suspension.getDuration() != null) {
                 BigDecimal duration = DatabaseObjectCommon.getValueForTimeFromMilliseconds(suspension.getDuration(), suspension.getDurationTimeUnit());
                 if (duration != null) outputString.append(duration.stripTrailingZeros().toPlainString());
@@ -292,7 +293,7 @@ public class SuspensionDetails extends HttpServlet {
             else outputString.append("N/A <br>");
             
             if ((suspension.isOneTime() != null) && suspension.isOneTime()) {
-                outputString.append("<b>Suspension ends at</b> = ");
+                outputString.append("<b>Suspension Ends At:</b> ");
                 if (suspension.getDeleteAtTimestamp() != null) {
                     String deleteAtTimestampString = DateAndTime.getFormattedDateAndTime(suspension.getDeleteAtTimestamp(), "yyyy-MM-dd, h:mm:ss a");
                     outputString.append(deleteAtTimestampString).append("<br>");
@@ -303,43 +304,43 @@ public class SuspensionDetails extends HttpServlet {
             if ((suspension.isOneTime() != null) && !suspension.isOneTime()) {
                 outputString.append("<br>");
                 
-                outputString.append("<b>Recur Sunday?</b> = ");
+                outputString.append("<b>Recur Sunday? :</b> ");
                 String isRecurSunday = "No";
                 if ((suspension.isRecurSunday() != null) && suspension.isRecurSunday()) isRecurSunday = "Yes";
                 if (suspension.isRecurSunday() != null) outputString.append(isRecurSunday).append("<br>");
                 else outputString.append("N/A <br>");
 
-                outputString.append("<b>Recur Monday?</b> = ");
+                outputString.append("<b>Recur Monday? :</b> ");
                 String isRecurMonday = "No";
                 if ((suspension.isRecurMonday() != null) && suspension.isRecurMonday()) isRecurMonday = "Yes";
                 if (suspension.isRecurMonday() != null) outputString.append(isRecurMonday).append("<br>");
                 else outputString.append("N/A <br>");
 
-                outputString.append("<b>Recur Tuesday?</b> = ");
+                outputString.append("<b>Recur Tuesday? :</b> ");
                 String isRecurTuesday = "No";
                 if ((suspension.isRecurTuesday() != null) && suspension.isRecurTuesday()) isRecurTuesday = "Yes";
                 if (suspension.isRecurTuesday() != null) outputString.append(isRecurTuesday).append("<br>");
                 else outputString.append("N/A <br>");
 
-                outputString.append("<b>Recur Wednesday?</b> = ");
+                outputString.append("<b>Recur Wednesday? :</b> ");
                 String isRecurWednesday = "No";
                 if ((suspension.isRecurWednesday() != null) && suspension.isRecurWednesday()) isRecurWednesday = "Yes";
                 if (suspension.isRecurWednesday() != null) outputString.append(isRecurWednesday).append("<br>");
                 else outputString.append("N/A <br>");
 
-                outputString.append("<b>Recur Thursday?</b> = ");
+                outputString.append("<b>Recur Thursday? :</b> ");
                 String isRecurThursday = "No";
                 if ((suspension.isRecurThursday() != null) && suspension.isRecurThursday()) isRecurThursday = "Yes";
                 if (suspension.isRecurThursday() != null) outputString.append(isRecurThursday).append("<br>");
                 else outputString.append("N/A <br>");
 
-                outputString.append("<b>Recur Friday?</b> = ");
+                outputString.append("<b>Recur Friday? :</b> ");
                 String isRecurFriday = "No";
                 if ((suspension.isRecurFriday() != null) && suspension.isRecurFriday()) isRecurFriday = "Yes";
                 if (suspension.isRecurFriday() != null) outputString.append(isRecurFriday).append("<br>");
                 else outputString.append("N/A <br>");
 
-                outputString.append("<b>Recur Saturday?</b> = ");
+                outputString.append("<b>Recur Saturday? :</b> ");
                 String isRecurSaturday = "No";
                 if ((suspension.isRecurSaturday() != null) && suspension.isRecurSaturday()) isRecurSaturday = "Yes";
                 if (suspension.isRecurSaturday() != null) outputString.append(isRecurSaturday).append("<br>");
